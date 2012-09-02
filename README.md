d1574239e41afafab216107e87cb08df1ed2e05b
Jack Dempsey <jack.dempsey@gmail.com>
2012-09-01 22:04:10 -0700

    beginnings

---
+++ README.md
@@ -0,0 +1,22 @@
+# ReadMe
+
+
+## Deployment
+
+```
+ey deploy
+```
+Remove
+## Thanks
+
+The original scaffold for this application was created by [App Scrolls](http://appscrolls.org).
+
+The project was created with the following scrolls:
+
+* active_admin
+* postgresql
+* rails_basics
+* simple_form
+* thin
+* twitter_bootstrap
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberLearnings::Application.config.secret_token = 'a90452d8a80863d6bdd5a515e29546b702bfb29f12f9f0ca3b6377532e50795aa278ad4cbf514161800ba94e0a545d0401dc2a7837ce192d5e8172816443e535'
